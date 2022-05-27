d5242afd1753b15794af230424ce8344aed7586b
gusdnd01 <cestop42@naver.com>
2022-05-27 23:47:32 +0900

    .
    .

--- Assets/01.Scripts/Hyunwoong/Scene/CurtainMove.cs
+++ Assets/01.Scripts/Hyunwoong/Scene/CurtainMove.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class CurtainMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Vector2 moveDir;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
